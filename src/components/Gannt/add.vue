2d24a66dc98c8d73a1602bf0aaf81168d3deb392
wangyong <573101013@qq.com>
2019-08-19 19:05:44 +0800

    项目计划

--- src/components/Gannt/add.vue
+++ src/components/Gannt/add.vue
@@ -5,12 +5,19 @@
 <script>
 import '../dhtmlx-gantt'
 import "../dhtmlx-gantt/codebase/locale/locale_cn";
+import Bus from "@/utils/eventBus.js";
 export default {
   props: {
     tasks: {
       type: Object,
       default () {
         return {data: [], links: []}
+      }
+    },
+    headerTitle: {
+      type: Array,
+      default () {
+        return []
       }
     }
   },
@@ -19,156 +26,130 @@ export default {
           workData : []
       }
   },
+  watch :{
+      'tasks':function(newValue,oldValue) {
+          gantt.clearAll();
+          gantt.parse(newValue)
+          this.$_initGanttEvents();
+      }
+  },
+  destroyed() {
+      Bus.$off("task-updated")
+      Bus.$off("link-updated")
+  },
   methods: {
     $_initGanttEvents: function () {
       if(gantt.$_eventsInitialized)
         return;
-        
-      gantt.attachEvent('onTaskSelected', (id) => {
-        console.log(id)
-        let task = gantt.getTask(id)
-        this.$emit('task-selected', task)
-      })
+      gantt.attachEvent("onTaskRowClick", (id, row) => {
+        console.log(row.outerHTML)
+          if(row.outerHTML.indexOf('新增')!=-1) {
+              Bus.$emit("task-updated",{id:id,operationType:'inserted'}); 
+          }
+          if(row.outerHTML.indexOf('编辑')!=-1) {
+              Bus.$emit("task-updated",{id:id,operationType:'updated'});
+          }
+          if(row.outerHTML.indexOf('删除')!=-1) {
+              Bus.$emit("task-updated",{id:id,operationType:'deleted'});
+          }
+          if(row.outerHTML.indexOf('复制')!=-1) {
+              Bus.$emit("task-updated",{id:id,operationType:'copy'});
+          }
+          if(row.outerHTML.indexOf('关联文档')!=-1) {
+              Bus.$emit("task-updated",{id:id,operationType:'archive'});
+          }
+          if(row.outerHTML.indexOf('关联风险')!=-1) {
+              Bus.$emit("task-updated",{id:id,operationType:'risk'});
+          }
+      });
 
-      gantt.attachEvent('onAfterTaskAdd', (id, task) => {
-        this.$emit('task-updated', id, 'inserted', task)
-        console.log("su")
-        console.log(task)
-        task.progress = task.progress || 0
-        if(gantt.getSelectedId() == id) {
-          this.$emit('task-selected', task)
+      gantt.attachEvent("onGridHeaderClick", (name, e) => {
+        if(name === 'addNode') {
+            Bus.$emit("task-updated",{id:0,operationType:'add'}); 
         }
-      })
-
-      gantt.attachEvent('onAfterTaskUpdate', (id, task) => {
-        console.log(task)
-        this.$emit('task-updated', id, 'updated', task)
-      })
-
-      gantt.attachEvent('onAfterTaskDelete', (id) => {
-        this.$emit('task-updated', id, 'deleted')
-        if(!gantt.getSelectedId()) {
-          this.$emit('task-selected', null)
-        }
-      })
+      });
 
       gantt.attachEvent('onAfterLinkAdd', (id, link) => {
-        this.$emit('link-updated', id, 'inserted', link)
+        Bus.$emit("link-updated",{id:id,operationType:'inserted',link:link}); 
       })
 
       gantt.attachEvent('onAfterLinkUpdate', (id, link) => {
-        this.$emit('link-updated', id, 'updated', link)
+        Bus.$emit("link-updated",{id:id,operationType:'updated',link:link}); 
       })
 
       gantt.attachEvent('onAfterLinkDelete', (id, link) => {
-        this.$emit('link-updated', id, 'deleted')
+        Bus.$emit("link-updated",{id:id,operationType:'deleted'}); 
       })
-      
+
       // gantt.eachParent(function(task){
       //   console.log(111111);
       // })
 
       gantt.$_eventsInitialized = true;
     },
-    getParent() {
-      console.log(this.workData)
-    }
+    getConfig() {
+        let _this = this
+        gantt.config.readonly = false;
+        // 设置x轴的时间显示格式
+        gantt.config.start_on_monday = true;
+        gantt.config.scale_unit = "month";
+        gantt.config.date_scale = "%Y-%m";
+        gantt.config.xml_date = "%Y-%m-%d %H:%i";
+        gantt.config.scale_height = 50;
+        gantt.config.subscales = [
+            {unit: "week", step: 1, date: "第%w周"},
+        ];
+        gantt.config.columns = this.headerTitle
+        // 拖拽task的时候，自动将起始时间和结束时间矫正为工作日备注：起始和结束时间遇到周末的时候，自动把时间改为工作日的时间 默认false
+        gantt.config.work_time = false;
+        gantt.config.correct_work_time = true;
+
+        // 允许通过拖拽的方式新增任务依赖的线条
+         gantt.config.drag_links = false;
+      //当task的长度改变时，自动调整图表坐标轴区间用于适配task的长度    
+        gantt.config.fit_tasks = true; 
+        // 禁止拖拽
+        gantt.config.drag_move = false;
+        // 禁止双击
+        gantt.config.details_on_dblclick = false;
+        gantt.config.drag_resize = false;
+        // 启用排序功能，点击表头可以排序
+        gantt.config.sort = true;
+        gantt.config.task_height = 25;
+        //gantt.config.min_column_width = 150;
+        gantt.init(this.$refs.gantt)
+        gantt.clearAll();
+        gantt.parse(this.$props.tasks)
+        this.$_initGanttEvents();
+      }
   },
-    
+  created() {
+  },
   mounted () {
-    console.log(gantt)
-    let _this = this
-    gantt.config.readonly = false;
-    // 设置x轴的时间显示格式
-    gantt.config.start_on_monday = true;
-	  gantt.config.scale_unit = "month";
-    gantt.config.date_scale = "%Y-%m";
-    gantt.config.xml_date = "%Y-%m-%d %H:%i";
-    gantt.config.scale_height = 50;
-    gantt.config.subscales = [
-        {unit: "week", step: 1, date: "第%w周"},
-        // {unit: "day", step: 1, date: "%j, %D"}
-        // {unit: "day", step: 1, date: "%j"}
-    ];
-    // gantt.config.task_height = 25;
-
-
-    // 设置左侧任务名称
-    gantt.config.columns=[
-        {name:"text",       label:"任务名",  tree:true,width:'160' },
-        {name:"start_date", label:"开始时间", align: "center", width:'100'},
-        {name:"end_date",   label:"结束时间", align: "center", width:'100' },
-        // {name:"duration",   label:"间隔",   align: "center",   width:'60'},
-        // {name:"person",     label:"负责人",   align: "center",   width:'60'},
-        {name:"add",        label:"" },
-    ];
-
-    // 拖拽task的时候，自动将起始时间和结束时间矫正为工作日备注：起始和结束时间遇到周末的时候，自动把时间改为工作日的时间 默认false
-    gantt.config.work_time = true;
-    gantt.config.correct_work_time = true;
-
-    // 允许通过拖拽的方式新增任务依赖的线条
-    // gantt.config.drag_links = false;
-   //当task的长度改变时，自动调整图表坐标轴区间用于适配task的长度    
-    gantt.config.fit_tasks = true; 
-    // 设置弹窗内容
-    gantt.config.lightbox.sections = [
-       {name: "description", height: 70, map_to: "text", type: "textarea", focus: true},
-       {name: "time", height: 30, map_to: "auto", type: "time" },
-      // {name: "time", map_to: "auto", type: "duration"},
-      // {
-      //   name: "deadline", map_to: {start_date: "deadline"},
-      //   type: "duration_optional",
-      //   button: true,
-      //   single_date: true
-      // },
-	  	// {name: "baseline", map_to: 'person',height:100, type: "checkbox",options: [
-      // {key:"Low", label: "Low"}, 
-      // {key:"Normal", label: "Normal"}, 
-      // {key:"High", label: "High"} ]
-      // }
-  	];
-  // 新增内容 baseline 对应上面的name: "baseline"
-	// gantt.locale.labels.section_baseline = "负责人";
-
-    // 设置弹窗头部和底部高度 总和是90
-    gantt.config.lightbox_additional_height = 90;
-
-    // 左侧树型结构调整时，可以任意拖动任务到任意节点下
-    gantt.config.order_branch = true;
-    gantt.config.order_branch_free = true;
-    // 启用排序功能，点击表头可以排序
-    gantt.config.sort = true;
-    gantt.init(this.$refs.gantt)
-    gantt.clearAll();
-    gantt.parse(this.$props.tasks)
-    this.$_initGanttEvents();
-    var dp = gantt.createDataProcessor(function(mode, taskState, data, rowId) {
-      if (localStorage[mode]) {
-        _this.workData = JSON.parse(localStorage[mode]);
-      }
-
-      switch(taskState) {
-        case "create":
-          _this.workData.push(data);
-        break;
-        case "update":
-          var itemIndex = _this.workData.findIndex(function(entry, index) {
-            return entry.id == rowId;
-          });
-          _this.workData[itemIndex] = data;
-        break;
-        break;
-        case "delete":
-          var itemIndex = _this.workData.findIndex(function(entry, index) {
-            return entry.id == rowId;
-          });
-          _this.workData.splice(itemIndex, 1);
-        break;
-      }
-      
-      localStorage[mode] = JSON.stringify(_this.workData);
-    });
+    this.getConfig()
+    // var dp = gantt.createDataProcessor(function(mode, taskState, data, rowId) {
+    //   if(mode === 'link') {
+    //       switch(taskState) {
+    //         case "create":
+    //           _this.workData.push(data);
+    //         break;
+    //         case "update":
+    //           console.log(data)
+    //           var itemIndex = _this.workData.findIndex(function(entry, index) {
+    //             return entry.id == rowId;
+    //           });
+    //           _this.workData[itemIndex] = data;
+    //         break;
+    //         break;
+    //         case "delete":
+    //           var itemIndex = _this.workData.findIndex(function(entry, index) {
+    //             return entry.id == rowId;
+    //           });
+    //           _this.workData.splice(itemIndex, 1);
+    //         break;
+    //       }
+    //   }
+    // });
   }
 }
 </script>
