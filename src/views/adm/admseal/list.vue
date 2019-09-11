e8d100c1f7bde791252600816a0afd010aa24e22
lishengfang <877109068@qq.com>
2019-09-11 19:27:09 +0800

    请假用印、

--- src/views/adm/admseal/list.vue
+++ src/views/adm/admseal/list.vue
@@ -2,77 +2,67 @@
     <div class="app-container white-bg list-panel" v-cloak>
         <div class="opertion-box">
             <el-button type="primary" icon="el-icon-plus" size="small" @click="add()" style="margin-right:10px">创建</el-button>
-            <el-input v-model="searchParam.zy_input" placeholder="请输入" clearable class="zy_input" style="width:190px"></el-input>
+            <el-input v-model="searchParam.nameOrPhone" placeholder="请输入员工姓名或手机号" clearable class="zy_input" style="width:190px"></el-input>
             <el-button type="primary" icon="el-icon-search" size="small" @click="handleCurrentChange(1)">查询</el-button>
             <el-button type="primary" icon="el-icon-menu" size="small" @click="isShowMore = !isShowMore">更多查询<i :class="[isShowMore ? 'el-icon-caret-bottom' : 'el-icon-caret-top', 'el-icon--right'] "></i></el-button>
-            <el-button type="primary" icon="el-icon-refresh" size="small" @click="approvalTime=[];resetList()">重置</el-button>
-            <el-button type="primary" icon="el-icon-upload" size="small" @click="exportExcel()">导出</el-button>
+            <el-button type="primary" icon="el-icon-refresh" size="small" @click="createTime=[];resetList()">重置</el-button>
         </div>
         <!-- 展开更多查询开始 -->
         <el-collapse-transition>
-            <div class="search-box" v-show="isShowMore">              
+            <div class="search-box" v-show="isShowMore">
                 <div class="form-box">
-                                       <div class="form-group">
-                        <label class="control-label">主键</label>
+                    <div class="form-group">
+                        <label class="control-label">文件类别</label>
                         <div class="input-group">
-                            <el-input v-model="searchParam.id" placeholder="请输入主键"></el-input>
+                            <el-select v-model="searchParam.car" filterable clearable placeholder="全部">
+                                <el-option
+                                    v-for="item in cxList"
+                                    :key="item.value"
+                                    :label="item.text"
+                                    :value="item.value">
+                                </el-option>
+                            </el-select>
                         </div>
                     </div>
-                                       <div class="form-group">
-                        <label class="control-label">所属管理公司(组织表管理公司id)</label>
+                    <div class="form-group">
+                        <label class="control-label">加盖印章</label>
                         <div class="input-group">
-                            <el-input v-model="searchParam.companyId" placeholder="请输入所属管理公司(组织表管理公司id)"></el-input>
+                            <el-select v-model="searchParam.car" filterable clearable placeholder="全部">
+                                <el-option
+                                    v-for="item in cxList"
+                                    :key="item.value"
+                                    :label="item.text"
+                                    :value="item.value">
+                                </el-option>
+                            </el-select>
                         </div>
                     </div>
-                                       <div class="form-group">
-                        <label class="control-label">交付单位</label>
-                        <div class="input-group">
-                            <el-input v-model="searchParam.delivery" placeholder="请输入交付单位"></el-input>
+                    <div class="form-group">
+                        <label class="control-label">审批状态</label>
+                        <div class="input-group input-groups">
+                            <el-select v-model="searchParam.approvalStatus" filterable clearable placeholder="全部">
+                                <el-option label="审批中" :value="1"></el-option>
+                                <el-option label="已通过" :value="2"></el-option>
+                                <el-option label="已驳回" :value="3"></el-option>
+                                <el-option label="已作废" :value="4"></el-option>
+                            </el-select>
                         </div>
                     </div>
-                                       <div class="form-group">
-                        <label class="control-label">用印事由</label>
-                        <div class="input-group">
-                            <el-input v-model="searchParam.reason" placeholder="请输入用印事由"></el-input>
-                        </div>
-                    </div>
-                                       <div class="form-group">
-                        <label class="control-label">状态(1-审批中，2-已通过  3-已驳回 4-已作废)</label>
-                        <div class="input-group">
-                            <el-input v-model="searchParam.approvalStatus" placeholder="请输入状态(1-审批中，2-已通过  3-已驳回 4-已作废)"></el-input>
-                        </div>
-                    </div>
-                                       <div class="form-group">
-                        <label class="control-label">数据状态(0-删除，1-正常)</label>
-                        <div class="input-group">
-                            <el-input v-model="searchParam.status" placeholder="请输入数据状态(0-删除，1-正常)"></el-input>
-                        </div>
-                    </div>
-                                       <div class="form-group">
-                        <label class="control-label">申请人</label>
-                        <div class="input-group">
-                            <el-input v-model="searchParam.creater" placeholder="请输入申请人"></el-input>
-                        </div>
-                    </div>
-                                       <div class="form-group">
-                        <label class="control-label">修改人</label>
-                        <div class="input-group">
-                            <el-input v-model="searchParam.updater" placeholder="请输入修改人"></el-input>
-                        </div>
-                    </div>
-                                       <div class="form-group">
+                    <div class="form-group">
                         <label class="control-label">申请时间</label>
                         <div class="input-group">
-                            <el-input v-model="searchParam.createTime" placeholder="请输入申请时间"></el-input>
+                            <el-date-picker
+                                v-model="createTime"
+                                type="daterange"
+                                :editable="false"
+                                range-separator="至"
+                                start-placeholder="开始日期"
+                                end-placeholder="结束日期"
+                                value-format="yyyy-MM-dd">
+                            </el-date-picker>
                         </div>
                     </div>
-                                       <div class="form-group">
-                        <label class="control-label">修改时间</label>
-                        <div class="input-group">
-                            <el-input v-model="searchParam.updateTime" placeholder="请输入修改时间"></el-input>
-                        </div>
-                    </div>
-                                   </div>
+                </div>
             </div>
         </el-collapse-transition>
         <div class="division-line"></div>
@@ -83,17 +73,12 @@
                         <el-button v-show="showEditBtn" @click="edit(row.id)" type="text" size="small">编辑</el-button>
                     </template>
                 </el-table-column>
-                                <el-table-column prop="id" sortable show-overflow-tooltip min-width="100" label="主键"></el-table-column>
-                                <el-table-column prop="companyId" sortable show-overflow-tooltip min-width="100" label="所属管理公司(组织表管理公司id)"></el-table-column>
-                                <el-table-column prop="delivery" sortable show-overflow-tooltip min-width="100" label="交付单位"></el-table-column>
-                                <el-table-column prop="reason" sortable show-overflow-tooltip min-width="100" label="用印事由"></el-table-column>
-                                <el-table-column prop="approvalStatus" sortable show-overflow-tooltip min-width="100" label="状态(1-审批中，2-已通过  3-已驳回 4-已作废)"></el-table-column>
-                                <el-table-column prop="status" sortable show-overflow-tooltip min-width="100" label="数据状态(0-删除，1-正常)"></el-table-column>
-                                <el-table-column prop="creater" sortable show-overflow-tooltip min-width="100" label="申请人"></el-table-column>
-                                <el-table-column prop="updater" sortable show-overflow-tooltip min-width="100" label="修改人"></el-table-column>
-                                <el-table-column prop="createTime" sortable show-overflow-tooltip min-width="100" label="申请时间"></el-table-column>
-                                <el-table-column prop="updateTime" sortable show-overflow-tooltip min-width="100" label="修改时间"></el-table-column>
-                            </el-table>
+                <el-table-column prop="name" sortable show-overflow-tooltip min-width="100" label="文件名称"></el-table-column>
+                <el-table-column prop="type" sortable show-overflow-tooltip min-width="100" label="文件类别"></el-table-column>
+                <el-table-column prop="approvalStatusText" sortable show-overflow-tooltip min-width="100" label="申请状态"></el-table-column>
+                <el-table-column prop="creater" sortable show-overflow-tooltip min-width="100" label="申请人"></el-table-column>
+                <el-table-column prop="createTime" sortable show-overflow-tooltip min-width="100" label="申请时间"></el-table-column>
+            </el-table>
             <el-pagination
                 @size-change="handleSizeChange"
                 @current-change="handleCurrentChange"
@@ -121,14 +106,30 @@
                 listUrl: "adm/admseal",
                 showSearch: false,
                 showAddBtn: this.getCurrentUserAuthority("/admseal/save"),
-                showEditBtn: this.getCurrentUserAuthority("/admseal/edit")
+                showEditBtn: this.getCurrentUserAuthority("/admseal/edit"),
+                createTime: [],
             }
         },
         mounted() {
             this.getList();
+            this.getDict()
         },
         methods: {
-
+            getListBefore(params) {
+                if (this.createTime) {
+                    params.createTimeStart = this.createTime[0];
+                    params.createTimeEnd = this.createTime[1];
+                } else {
+                    params.createTimeStart = '';
+                    params.createTimeEnd = '';
+                }
+            },
+            getDict() {
+                let r = 'CX';
+                ajax.get("upms/dict/allType/"+r).then(rs => {
+                    this.cxList = rs.CX;
+                });
+            },
         }
     }
 </script>
