638c19395bc083dd612cf92c2bbb4ea7368d3464
suyong <suyong@bobandata>
2019-10-24 11:08:57 +0800

    im优化

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,8 +4,8 @@ const prodEnv = require("./prod.env");
 
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
-    // BASE_API: '"https://dev.xtbg.zdsxc.com/xtbg/"',
-    BASE_API: '"http://localhost:9063/"',
+    BASE_API: '"https://dev.xtbg.zdsxc.com/xtbg/"',
+    // BASE_API: '"http://localhost:9063/"',
     URL_API: '"https://dev.zdsxc.com/"',
     BASE_IP: '"dev.xtbg.zdsxc.com"',
     BASE_HOST:'443',
